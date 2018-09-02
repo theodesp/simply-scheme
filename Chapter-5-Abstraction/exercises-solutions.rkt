63e53102c65302df32995f5845468e5b21f3e281
Theo <theo.despoudis@teckro.com>
2018-09-02 19:14:40 +0100

    Cleanup

--- Chapter-5-Abstraction/exercises-solutions.rkt
+++ Chapter-5-Abstraction/exercises-solutions.rkt
@@ -1,35 +1,97 @@
-;#lang racket
-;(require (planet dyoo/simply-scheme:2:2))
+#lang racket
+(require (planet dyoo/simply-scheme:2:2))
 ;
 ;17.1 What will Scheme print in response to each of the following expressions? Try to figure it out in your head before
 ;you try it on the computer.
 ;> (car '(Rod Chris Colin Hugh Paul))
+
+; Answer: Rod
+
 ;> (cadr '(Rod Chris Colin Hugh Paul))
+
+; Answer: Chris
+
 ;> (cdr '(Rod Chris Colin Hugh Paul))
+
+; Answer: '(Chris Colin Hugh Paul)
+
 ;> (car 'Rod)
+
+; Throw error
+
 ;> (cons '(Rod Argent) '(Chris White))
+
+; Answer: '((Rod Argent) Chris White)
+
 ;> (append '(Rod Argent) '(Chris White))
+
+; Answer: '(Rod Argent Chris White)
+
+
 ;> (list '(Rod Argent) '(Chris White))
+
+; Answer: '((Rod Argent) (Chris White))
+
 ;> (caadr '((Rod Argent) (Chris White)
 ; (Colin Blunstone) (Hugh Grundy) (Paul Atkinson)))
+
+; Answer: 'Chris
+
 ;Page 300
 ;> (assoc 'Colin '((Rod Argent) (Chris White)
 ; (Colin Blunstone) (Hugh Grundy) (Paul Atkinson)))
+
+; Answer: '(Colin Blunstone)
+
 ;> (assoc 'Argent '((Rod Argent) (Chris White)
 ; (Colin Blunstone) (Hugh Grundy) (Paul Atkinson)))
+
+; Answer: #f
+
 ;17.2 For each of the following examples, write a procedure of two arguments that, when applied to the sample
 ;arguments, returns the sample result. Your procedures may not include any quoted data
 ;
 ;> (f1 '(a b c) '(d e f))
 ;((B C D))
+
+(define (f1 lst1 lst2)
+  (cons (append (cdr lst1) (list (car lst2))) '()))
+
+(f1 '(a b c) '(d e f))
+
 ;> (f2 '(a b c) '(d e f))
 ;((B C) E)
+
+(define (f2 lst1 lst2)
+   (cons (cdr lst1)
+         (list (cadr lst2))))
+
+(f2 '(a b c) '(d e f))
+
 ;> (f3 '(a b c) '(d e f))
 ;(A B C A B C)
+
+(define (f3 lst1 lst2)
+   (append lst1 lst1))
+
+(f3 '(a b c) '(d e f))
+
 ;> (f4 '(a b c) '(d e f))
 ;((A D) (B C E F))
+
+(define (f4 lst1 lst2)
+   (cons (list (car lst1) (car lst2))
+         (list (append (cdr lst1)  (cdr lst2)))))
+
+(f4 '(a b c) '(d e f))
+
 ;17.3 Describe the value returned by this invocation of map:
 ;> (map (lambda (x) (lambda (y) (+ x y))) '(1 2 3 4))
+
+
+; Answer: A list of procedures that accept 1 parameter and when invoked it will
+; print the accociated number + y in that list
+
 ;Real Exercises
 ;17.4 Describe the result of calling the following procedure with a list as its argument. (See if you can figure it out
 ;before you try it.)
@@ -39,21 +101,48 @@
 ; (if (null? lst)
 ; other
 ; (mystery–helper (cdr lst) (cons (car lst) other))))
+
+; Answer: Reverses a list
+
 ;Page 301
 ;17.5 Here's a procedure that takes two numbers as arguments and returns whichever number is larger:
 ;(define (max2 a b)
 ; (if (> b a) b a))
 ;Use max2 to implement max, a procedure that takes one or more numeric arguments and returns the largest of them.
+
+(define (max2 a b)
+  (if (> b a) b a))
+
+(define (max args)
+  (cond ((empty? args) '())
+        ((empty? (cdr args)) (car args))
+        (else (max2 (car args) (max (cdr args))))))
+
+(max '(1 2 30 2 7 19))
+
 ;17.6 Implement append using car, cdr, and cons. (Note: The built-in append can take any number of
 ;arguments. First write a version that accepts only two arguments. Then, optionally, try to write a version that takes any
 ;number.)
+
+
 ;17.7 Append may remind you of sentence. They're similar, except that append works only with lists as
 ;arguments, whereas sentence will accept words as well as lists. Implement sentence using append. (Note: The
 ;built-in sentence can take any number of arguments. First write a version that accepts only two arguments. Then,
 ;optionally, try to write a version that takes any number. Also, you don't have to worry about the error checking that the
 ;real sentence does.)
+
+
 ;17.8 Write member.
 ;17.9 Write list–ref.
+
+(define (list–ref lst i)
+  (cond ((empty? lst) '())
+        ((or (> i (length lst)) (< (length lst) i)) '())
+        ((and (= i 0) (> (length lst) 1)) (car lst))
+        (else (list–ref (cdr lst) (- i 1)))))
+
+(list–ref '(back in the ussr) 2)
+
 ;17.10 Write length.
 ;17.11 Write before–in–list?, which takes a list and two elements of the list. It should return #t if the second
 ;argument appears in the list argument before the third argument:
@@ -64,11 +153,31 @@
 ;#F
 ;The procedure should also return #f if either of the supposed elements doesn't appear at all.
 ;Page 302
+
+(define (before–in–list? lst a b)
+  (cond ((empty? lst) #f)
+        ((empty? (cdr lst)) #f)
+        ((and (equal? (car lst) a) (member? b (cdr lst))) #t)
+        (else (before–in–list? (cdr lst) a b))))
+
+(before–in–list? '(back in the ussr) 'in 'ussr)
+(before–in–list? '(back in the ussr) 'the 'back)
+
 ;17.12 Write a procedure called flatten that takes as its argument a list, possibly including sublists, but whose
 ;ultimate building blocks are words (not Booleans or procedures). It should return a sentence containing all the words
 ;of the list, in the order in which they appear in the original:
 ;> (flatten '(((a b) c (d e)) (f g) ((((h))) (i j) k)))
 ;(A B C D E F G H I J K)
+
+(define (flatten lst)
+  (cond ((not (list? lst)) (list lst))
+        ((empty? lst) '())
+        ((empty? (cdr lst)) (flatten (car lst)))
+        (else (append (flatten (car lst))
+                      (flatten (cdr lst))))))
+
+(flatten '(((a b) c (d e)) (f g) ((((h))) (i j) k)))
+
 ;17.13 Here is a procedure that counts the number of words anywhere within a structured list:
 ;(define (deep–count lst)
 ; (cond ((null? lst) 0)
@@ -76,6 +185,14 @@
 ; (else (+ (deep–count (car lst))
 ; (deep–count (cdr lst))))))
 ;Although this procedure works, it's more complicated than necessary. Simplify it.
+
+(define (deep–count lst)
+  (cond ((null? lst) 0)
+        ((word? (car lst)) (+ 1 (deep–count (cdr lst))))
+        (reduce + (map (lambda (sublist) (deep–count sublist)) lst))))
+
+(deep–count '(1 2 1))
+
 ;17.14 Write a procedure branch that takes as arguments a list of numbers and a nested list structure. It should be the
 ;list-of-lists equivalent of item, like this:
 ;> (branch '(3) '((a b) (c d) (e f) (g h)))
@@ -88,6 +205,19 @@
 ;((C D) (E F) ((G H) (I J)) K)
 ;The third element of that smaller list is ((G H) (I J)); the first element of that is (G H); and the second element
 ;of that is just H.
+
+(define (branch lst container)
+  (cond
+    ((empty? lst) '())
+    ((empty? container) '())
+    ((and (= (length lst) 1) (list-ref container (- (car lst) 1))))
+    ((= (length (cdr lst)) 1) (list-ref (list-ref container (- (car lst) 1)) (- (cadr lst) 1)))
+    (else (branch (cdr lst) (list-ref container (- (car lst) 1))))))
+
+(branch '(3) '((a b) (c d) (e f) (g h)))
+(branch '(3 2) '((a b) (c d) (e f) (g h)))
+(branch '(2 3 1 2) '((a b) ((c d) (e f) ((g h) (i j)) k) (l m)))
+
 ;17.15 Modify the pattern matcher to represent the known–values database as a list of two-element lists, as we
 ;suggested at the beginning of this chapter.
 ;
@@ -98,3 +228,24 @@
 ;#T
 ;> (valid–infix? '(4 + 3 * (5 2)))
 ;#F
+
+(define (operator? wd)
+  (member? wd '*-+/))
+
+(define (valid–infix? expr)
+  (cond ((empty? expr) #t)
+        ((= (length expr) 1) #t)
+        ((list? (car expr)) (and (valid–infix? (car expr))
+                                 (valid–infix? (cdr expr))))
+        ((list? (cadr expr)) (and (valid–infix? (cadr expr))
+                                  (valid–infix? (cddr expr))))
+        ((and (= (length expr) 3)
+              (list? (caddr expr))) (valid–infix? (caddr expr)))
+        ((and (number? (car expr))
+              (operator? (cadr expr))
+              (number? (caddr expr))
+              (valid–infix? (cddr expr))))
+         (else #f)))
+
+(valid–infix? '(3 * (5 - 2)))
+(valid–infix? '(4 + 3 * (5 2)))
